b4228e73ca877422976b58f365b3108aab950d64
tataurova <tataurovaoe@yandex.ru>
2020-04-12 01:28:32 +0300

    Разделение шаблонов и данных

---
+++ src/mock/filter.js
@@ -0,0 +1,12 @@
+const filterNames = [`all`, `overdue`, `today`, `favorites`, `repeating`, `archive`];
+
+const generateFilters = () => {
+  return filterNames.map((it) => {
+    return {
+      name: it,
+      count: Math.floor(Math.random() * 10),
+    };
+  });
+};
+
+export {generateFilters};
